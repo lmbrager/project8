0789272a4f40615437fd44bda7ee40f86dd7fc47
Laney M. Brager <lmbrager@cs-parallel.ua.edu>
2020-04-17 19:40:06 -0500

    initial

---
+++ rules.pl
@@ -0,0 +1,10 @@
+payment(First, Last, Pay) :- commission(F, L, Min, Sales, Crate),
+	Sales*Crate > Min,
+	P is Sales*Crate.
+payment(First, Last, Pay) :- commission(F, L, Min, Sales, Crate),
+	Sales*Crate =< Min,
+	P is Min.
+%payment(First, Last, Pay) :- salaried(F, L, Wage),
+%	P is Wage.
+%payment(First, Last, Pay) :- hourly(F, L, Hours, Rate),
+%	P is Hours*Rate.
