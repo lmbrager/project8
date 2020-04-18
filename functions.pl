73a68b36846991c05543ee4067bd3719b45c7111
lmbrager <43522164+lmbrager@users.noreply.github.com>
2020-04-17 21:47:37 -0500

    Update functions.pl

--- functions.pl
+++ functions.pl
@@ -1,20 +1,12 @@
-%list(Op, Ref, List) :- (
-%	Op='eq', findall([X,Y,Z],(payment(X,Y,Z),Z=:=Ref),List);
-%	Op='ne', findall([X,Y,Z],(payment(X,Y,Z),Z=\=Ref),List);
-%	Op='gt', findall([X,Y,Z],(payment(X,Y,Z),Z>Ref),List);
-%	Op='ge', findall([X,Y,Z],(payment(X,Y,Z),Z>=Ref),List);
-%	Op='lt', findall([X,Y,Z],(payment(X,Y,Z),Z<Ref),List);
-%	Op='le', findall([X,Y,Z],(payment(X,Y,Z),Z=<Ref),List)
-%).
-
-%count(Op, Ref, Count) :- (
-%	Op='eq', findall(Z, (payment(X, Y, Z),Z=:=Ref), List), length(List, Count);
-%	Op='ne', findall(Z, (payment(X, Y, Z),Z=\=Ref), List), length(List, Count);
-%	Op='gt', findall(Z, (payment(X, Y, Z),Z>Ref), List), length(List, Count);
-%	Op='ge', findall(Z, (payment(X, Y, Z),Z>=Ref), List), length(List, Count);
-%	Op='lt', findall(Z, (payment(X, Y, Z),Z<Ref), List), length(List, Count);
-%	Op='le', findall(Z, (payment(X, Y, Z),Z=<Ref), List), length(List, Count)
-%).
+max(Op, Ref, Max) :- (
+                Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
+                Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
+                Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
+                Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
+                Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
+                Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
+),
+max_list(List, Max).
 
 count(Op, Ref, Count) :- (
                 Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List),  length(List, Count);
