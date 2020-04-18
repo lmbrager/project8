a8d1c3f66cca37ae8f9b39492515b34f9945e865
lmbrager <43522164+lmbrager@users.noreply.github.com>
2020-04-17 22:40:35 -0500

    Update functions.pl

--- functions.pl
+++ functions.pl
@@ -8,6 +8,16 @@ max(Op, Ref, Max) :- (
 ),
 max_list(List, Max).
 
+min(Op, Ref, Max) :- (
+                Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
+                Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
+                Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
+                Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
+                Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
+                Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
+),
+min_list(List, Min).
+
 print(Op,Ref,Max) :- (
     Op='eq', findall([X, Y, Z], (payment(X, Y, Z), Z=:=Ref), List);
     Op='ne', findall([X, Y, Z], (payment(X, Y, Z), Z=\=Ref), List);
