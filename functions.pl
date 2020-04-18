6bc8d9d6a7af6b2b068658fd0b2692c6ad7d0b35
Laney M. Brager <lmbrager@cs-parallel.ua.edu>
2020-04-17 23:17:27 -0500

    finished

--- functions.pl
+++ functions.pl
@@ -1,57 +1,54 @@
 max(Op, Ref, Max) :- (
-                Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
-                Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
-                Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
-                Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
-                Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
-                Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
+	Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
+	Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
+	Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
+	Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
+	Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
+	Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
 ),
 max_list(List, Max).
 
 min(Op, Ref, Min) :- (
-                Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
-                Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
-                Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
-                Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
-                Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
-                Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
+	Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
+	Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
+	Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
+	Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
+	Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
+	Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
 ),
 min_list(List, Min).
 
 total(Op, Ref, Sum) :- (
-                Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
-                Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
-                Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
-                Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
-                Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
-                Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
+	Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
+	Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
+	Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
+	Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
+	Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
+	Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
 ),
 sum_list(List, Sum).
 
-average(Op, Ref, Avg) :- (
-                Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List);
-                Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List);
-                Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List);
-                Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List);
-                Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List);
-                Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List)
-),
-sum_list(List, Avg).
+avg(Op, Ref, Avg) :- (
+	total(Op, Ref, Sum),
+	count(Op, Ref, Count),
+	Count > 0 -> Avg is Sum/Count
+	; Avg is 0
+).
 
-print(Op,Ref,Max) :- (
-    Op='eq', findall([X, Y, Z], (payment(X, Y, Z), Z=:=Ref), List);
-    Op='ne', findall([X, Y, Z], (payment(X, Y, Z), Z=\=Ref), List);
-    Op='gt', findall([X, Y, Z], (payment(X, Y, Z), Z>Ref), List);
-    Op='ge', findall([X, Y, Z], (payment(X, Y, Z), Z>=Ref), List);
-    Op='lt', findall([X, Y, Z], (payment(X, Y, Z), Z<Ref), List);
-    Op='le', findall([X, Y, Z], (payment(X, Y, Z), Z=<Ref), List)
+list(Op,Ref,List) :- (
+	Op='eq', findall([X, Y, Z], (payment(X, Y, Z), Z=:=Ref), List);
+	Op='ne', findall([X, Y, Z], (payment(X, Y, Z), Z=\=Ref), List);
+	Op='gt', findall([X, Y, Z], (payment(X, Y, Z), Z>Ref), List);
+	Op='ge', findall([X, Y, Z], (payment(X, Y, Z), Z>=Ref), List);
+	Op='lt', findall([X, Y, Z], (payment(X, Y, Z), Z<Ref), List);
+	Op='le', findall([X, Y, Z], (payment(X, Y, Z), Z=<Ref), List)
 ).
 
 count(Op, Ref, Count) :- (
-                Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List),  length(List, Count);
-                Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List),  length(List, Count);
-                Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List),  length(List, Count);
-                Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List),  length(List, Count);
-                Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List),  length(List, Count);
-                Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List),  length(List, Count)
+	Op='eq', findall(Z, (payment(X, Y, Z), Z=:=Ref), List),  length(List, Count);
+	Op='ne', findall(Z, (payment(X, Y, Z), Z=\=Ref), List),  length(List, Count);
+	Op='gt', findall(Z, (payment(X, Y, Z), Z>Ref), List),  length(List, Count);
+	Op='ge', findall(Z, (payment(X, Y, Z), Z>=Ref), List),  length(List, Count);
+	Op='lt', findall(Z, (payment(X, Y, Z), Z<Ref), List),  length(List, Count);
+	Op='le', findall(Z, (payment(X, Y, Z), Z=<Ref), List),  length(List, Count)
           ).
