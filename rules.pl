3edd16f40765c7ae222af397caee25d06f500eec
lmbrager <43522164+lmbrager@users.noreply.github.com>
2020-04-17 19:41:30 -0500

    Update rules.pl

--- rules.pl
+++ rules.pl
@@ -1,9 +1,9 @@
-payment(First, Last, Pay) :- commission(F, L, Min, Sales, Crate),
-	Sales*Crate > Min,
-	P is Sales*Crate.
-payment(First, Last, Pay) :- commission(F, L, Min, Sales, Crate),
-	Sales*Crate =< Min,
-	P is Min.
+payment(F, L, P) :- commission(F, L, Min, Sales, Crate),
+                    Sales*Crate > Min,
+                    P is Sales*Crate.
+payment(F, L, P) :- commission(F, L, Min, Sales, Crate),
+                    Sales*Crate =< Min,
+                    P is Min.
 %payment(First, Last, Pay) :- salaried(F, L, Wage),
 %	P is Wage.
 %payment(First, Last, Pay) :- hourly(F, L, Hours, Rate),
