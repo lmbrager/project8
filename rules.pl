f759864392d3606c8785eb2cc2fed93751c595c9
lmbrager <43522164+lmbrager@users.noreply.github.com>
2020-04-17 22:21:25 -0500

    Update rules.pl

--- rules.pl
+++ rules.pl
@@ -4,7 +4,14 @@ payment(F, L, P) :- commission(F, L, Min, Sales, Crate),
 payment(F, L, P) :- commission(F, L, Min, Sales, Crate),
                     Sales*Crate =< Min,
                     P is Min.
-%payment(First, Last, Pay) :- salaried(F, L, Wage),
-%	P is Wage.
-%payment(First, Last, Pay) :- hourly(F, L, Hours, Rate),
-%	P is Hours*Rate.
+payment(F, L, P) :- salaried(F, L, Wage),
+	P is Wage.
+payment(F, L, P) :- hourly(F, L, Hours, Rate),
+  Hours =< 40,
+  P is Hours*Rate.
+payment(F, L, P) :- hourly(F, L, Hours, Rate),
+  Hours =< 40, Hours>40,
+  P is 40*Rate + (Hours-40)*Rate*1.5.
+payment(F, L, P) :- hourly(F, L, Hours, Rate),
+  Hours > 50,
+  P is 40*Rate + 15*Rate + 2*Rate*(Hours-50).
